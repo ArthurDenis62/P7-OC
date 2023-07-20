0344c154f0490444bf15089d1a3bd2dd1535cf4b
ArthurDenis62 <arthurdenis.pro@gmail.com>
2023-07-20 13:10:09 +0200

    First

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './my-sass.scss'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './pages/Layout';
+import Home from './pages/Home';
+import Logement from './pages/Logement';
+import About from './pages/About';
+import Error from './pages/Error';
+
+export default function App () {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path="Logement" element={<Logement />} />
+                    <Route path="About" element={<About />} />
+                    <Route path="*" element={<Error />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
---
+++ src/pages/Home.js
@@ -0,0 +1,8 @@
+import bgAccueil from '../img/bgAccueil.svg'
+
+const Home = () => {
+    return (
+    )
+}
+
+export default Home;
